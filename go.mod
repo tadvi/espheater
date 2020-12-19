089d9e3878d8119dc87da03d45b3b700ef02e859
Tad Vizbaras <tvizbaras@performline.com>
2020-12-19 07:26:45 -0500

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/tadvi/espheater
+
+go 1.13
+
+require (
+	github.com/lucperkins/rek v0.1.3
+	github.com/tadvi/log v0.0.0-20201215194430-a2e530b1aafb
+)
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"time"
+
+	"github.com/lucperkins/rek"
+	"github.com/tadvi/log"
+)
+
+const timeout = 15
+
+var (
+	esp      string
+	lowTemp  int
+	heatTime int
+)
+
+type ESPMessage struct {
+	Message     string `json:"message"`
+	Temperature int    `json:"temperature"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Hardware    string `json:"hardware"`
+	Connected   bool   `json:"connected"`
+}
+
+func init() {
+	flag.StringVar(&esp, "esp", "ESP_4E2ABA", "ESP8266 device address")
+
+	flag.IntVar(&lowTemp, "low-temp", 21, "low temprature to turn heater on")
+	flag.IntVar(&heatTime, "heat-time", 10, "heat time")
+}
+
+func main() {
+	flag.Parse()
+
+	esp = "http://" + esp
+
+	for {
+		if time.Now().Local().Minute() == 0 {
+
+			res, err := rek.Get(esp+"/temperature", rek.Timeout(timeout*time.Second))
+			if err != nil {
+				log.Errorf("Error getting temperature: %v", err)
+				continue
+			}
+			if res.StatusCode() >= 300 {
+				log.Errorf("Error getting temperature: %v", err, res.StatusCode())
+				continue
+			}
+			defer res.Body().Close()
+
+			var msg ESPMessage
+			bs, err := ioutil.ReadAll(res.Body())
+			if err != nil {
+				log.Errorf("Error reading request: %v", err)
+				continue
+			}
+
+			if err := json.Unmarshal(bs, &msg); err != nil {
+				log.Errorf("Error parsing request: %v", err)
+				log.Errorf("Dump: %+v", msg)
+				continue
+			}
+
+			log.Debugf("Dump: %+v", msg)
+
+			if msg.Temperature-4 < lowTemp {
+
+				res, err := rek.Get(esp+"/digital/8/1", rek.Timeout(timeout*time.Second))
+				if err != nil {
+					log.Errorf("Error turning on the heater: %v", err)
+					continue
+				}
+				res.Body().Close()
+
+				time.Sleep(time.Minute * time.Duration(heatTime))
+
+				res, err = rek.Get(esp+"/digital/8/0", rek.Timeout(timeout*time.Second))
+				if err != nil {
+					log.Errorf("Error turning off the heater: %v", err)
+					continue
+				}
+				res.Body().Close()
+
+				log.Infof("Heater worked")
+			}
+		}
+
+		time.Sleep(time.Minute)
+	}
+}
